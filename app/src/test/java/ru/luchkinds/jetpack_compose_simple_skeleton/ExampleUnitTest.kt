da1a9297798069ff78f0feb33f5d06b560c42621
LuchkinDS <LuchkinDS@gmail.com>
2024-10-07 22:33:51 +0700

    init commit

---
+++ app/src/test/java/ru/luchkinds/jetpack_compose_simple_skeleton/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package ru.luchkinds.jetpack_compose_simple_skeleton
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
