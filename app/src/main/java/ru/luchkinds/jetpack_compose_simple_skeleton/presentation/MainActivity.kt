decd5b181bead762c084d3198780ef934717b321
LuchkinDS <LuchkinDS@gmail.com>
2024-10-13 20:10:33 +0700

    1. add @AndroidEntryPoint to MainActivity 2. upgrade kotlin version 2.0.10 -> 2.0.20

--- app/src/main/java/ru/luchkinds/jetpack_compose_simple_skeleton/presentation/MainActivity.kt
+++ app/src/main/java/ru/luchkinds/jetpack_compose_simple_skeleton/presentation/MainActivity.kt
@@ -14,11 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 import ru.luchkinds.jetpack_compose_simple_skeleton.navigation.SampleNavigation
 import ru.luchkinds.jetpack_compose_simple_skeleton.ui.theme.JetpackComposeSimpleSkeletonTheme
 
-@EntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
