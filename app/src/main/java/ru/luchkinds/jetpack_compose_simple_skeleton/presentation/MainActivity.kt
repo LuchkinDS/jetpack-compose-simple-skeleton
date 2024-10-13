b2aa047742d484df2d0f297acf4e2a31131fb59f
LuchkinDS <LuchkinDS@gmail.com>
2024-10-13 19:14:16 +0700

    1. add @EntryPoint to MainActivity

--- app/src/main/java/ru/luchkinds/jetpack_compose_simple_skeleton/presentation/MainActivity.kt
+++ app/src/main/java/ru/luchkinds/jetpack_compose_simple_skeleton/presentation/MainActivity.kt
@@ -14,9 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.EntryPoint
 import ru.luchkinds.jetpack_compose_simple_skeleton.navigation.SampleNavigation
 import ru.luchkinds.jetpack_compose_simple_skeleton.ui.theme.JetpackComposeSimpleSkeletonTheme
 
+@EntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
